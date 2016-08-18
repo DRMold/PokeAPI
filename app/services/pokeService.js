ecf94f97c37ff7f79b2d0f049a23a8f16e8de998
Cameron Bowie <cameron.bowie@parivedasolutions.com>
2016-08-18 13:20:22 -0500

    modified so getPokemon takes id

--- app/services/pokeService.js
+++ app/services/pokeService.js
@@ -1,6 +1,12 @@
 app.service("pokeService", ["$http", function($http) {
   var vm = this;
-  vm.getPokemon = function() {
-    return $http.get("http://pokeapi.co/api/v2/pokemon/12");
+
+  vm.getPokemon = function(id) {
+    return $http.get("http://pokeapi.co/api/v2/pokemon/" + id);
   }
+
+  vm.getAllPokemon = function() {
+  	return;
+  }
+
 }]);
